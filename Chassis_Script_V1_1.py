f66f703a8ccd96188edcffef16a83dac12ff05b8
siri-dhillon <sirpreetdhillon97@gmail.com>
2022-09-25 14:36:49 -0700

    editing some minor python syntax errors

--- Chassis_Script_V1_1.py
+++ Chassis_Script_V1_1.py
@@ -24,15 +24,18 @@ GPIO.setup(PIN_DIR_RIGHT, GPIO.OUT)
 
 
 # Function to move the robot forward 
-def robot_fwd(PWMFREQ)
+def robot_fwd(PWMFREQ):
+	print(" forward function has been entered")
 	pwm_left = GPIO.PWM(PIN_SPEED_LEFT,PWMFREQ)
+	print(" PWMFREQ value is: ", PWMFREQ)
+	print(" pwm_left PIN is: ", PIN_SPEED_LEFT)
 	pwm_right = GPIO.PWM(PIN_SPEED_RIGHT,PWMFREQ)
 	GPIO.output(PIN_DIR_LEFT, GPIO.LOW)
 	GPIO.output(PIN_DIR_RIGHT, GPIO.HIGH)
 	
 	
 # Function to move the robot backwards 
-def robot_rev(PWMFREQ)
+def robot_rev(PWMFREQ):
 	pwm_left = GPIO.PWM(PIN_SPEED_LEFT,PWMFREQ)
 	pwm_right = GPIO.PWM(PIN_SPEED_RIGHT,PWMFREQ)
 	GPIO.output(PIN_DIR_LEFT, GPIO.HIGH)
@@ -40,33 +43,39 @@ def robot_rev(PWMFREQ)
 
 
 # Function to turn right
-def robot_turn_right(PWMFREQ_LEFT, PWMFREQ_RIGHT)
+def robot_turn_right(PWMFREQ_LEFT, PWMFREQ_RIGHT):
 	pwm_left = GPIO.PWM(PIN_SPEED_LEFT,PWMFREQ_LEFT)
 	pwm_right = GPIO.PWM(PIN_SPEED_RIGHT,PWMFREQ_RIGHT)
 	GPIO.output(PIN_DIR_LEFT, GPIO.LOW)
 	GPIO.output(PIN_DIR_RIGHT, GPIO.HIGH)
 
 # Function to turn left
-def robot_turn_left(PWMFREQ_LEFT, PWMFREQ_RIGHT)
+def robot_turn_left(PWMFREQ_LEFT, PWMFREQ_RIGHT):
 	pwm_left = GPIO.PWM(PIN_SPEED_LEFT,PWMFREQ_LEFT)
 	pwm_right = GPIO.PWM(PIN_SPEED_RIGHT,PWMFREQ_RIGHT)
 	GPIO.output(PIN_DIR_LEFT, GPIO.LOW)
 	GPIO.output(PIN_DIR_RIGHT, GPIO.HIGH)
 	
 # Function to stop the robot
-def robot_stop()
+def robot_stop():
+	pwm_left = GPIO.PWM(PIN_SPEED_LEFT, 1)
+	pwm_right = GPIO.PWM(PIN_SPEED_RIGHT, 1)
 	pwm_left.stop()
 	pwm_right.stop()
 	
 # Function to increase speed (where inc is a positive value to increment by)
-def robot_inc_speed(min_dc, max_dc, inc)
+def robot_inc_speed(min_dc, max_dc, inc):
+	pwm_left = GPIO.PWM(PIN_SPEED_LEFT, 1)
+	pwm_right = GPIO.PWM(PIN_SPEED_RIGHT, 1)
 	for duty in range (min_dc, max_dc, inc):
 		pwm_left.ChangeDutyCycle(duty)
 		pwm_right.ChangeDutyCycle(duty)
 		time.sleep(0.1)
 		
 # Function to decrease speed (where dec is a positive value to decrement by)
-def robot_dec_speed(min_dc, max_dc, dec)
+def robot_dec_speed(min_dc, max_dc, dec):
+	pwm_left = GPIO.PWM(PIN_SPEED_LEFT, 1)
+	pwm_right = GPIO.PWM(PIN_SPEED_RIGHT, 1)
 	for duty in range (max_dc, min_dc, dec):
 		pwm_left.ChangeDutyCycle(duty)
 		pwm_right.ChangeDutyCycle(duty)
--- WiE_Design_Master_Script.py
+++ WiE_Design_Master_Script.py
@@ -12,8 +12,7 @@ PIN_DIR_RIGHT = 16
 PIN_SPEED_LEFT = 32
 PIN_SPEED_RIGHT = 12
 
-pwm_left = GPIO.PWM(PIN_SPEED_LEFT,0) 
-pwm_right = GPIO.PWM(PIN_SPEED_RIGHT,0)
+
 
 
 # Import GPIO Library
@@ -21,13 +20,19 @@ import RPi.GPIO as GPIO
 import time
 
 # Import Robot Test Files
-import Chassis_Script as chassis
+import Chassis_Script_V1_1 as chassis
+
+# setup for pins -- SIRI COMMENT --> change it to appropriate later 
+# pwm_left = GPIO.PWM(PIN_SPEED_LEFT,10) 
+# pwm_right = GPIO.PWM(PIN_SPEED_RIGHT,10)
 
 # Disable warnings
 GPIO.setwarnings(False)
+print("warnings off done")
 
 # Set pi to use pins
 GPIO.setmode(GPIO.BOARD)
+print("board mode is set")
 
 
 
@@ -35,12 +40,12 @@ GPIO.setmode(GPIO.BOARD)
 # Example For Chassis Testing
 
 # Change PWMFREQ should be changed for moving fwd and bckwd
-PWMFREQ = 0
+PWMFREQ = 50
 
 # Change PWMFREQ_LEFT and PWMFREQ_RIGHT for turning 
 # The smaller value should be the direction to turn towards
-PWMFREQ_RIGHT = 0
-PWMFREQ_LEFT = 0
+PWMFREQ_RIGHT = 50
+PWMFREQ_LEFT = 50
 
 # For gradually increasing and decreasing speed
 MIN_DUTY_CYCLE = 10
@@ -49,21 +54,24 @@ MAX_DUTY_CYCLE = 90
 INCREMENT_BY = 8
 DECREMENT_BY = 5
 
-
+print("starting forward")
 chassis.robot_fwd(PWMFREQ)
-robot_stop()
+print(" forward done ")
+print(" stopping... ")
+chassis.robot_stop()
+print(" stopped ")
 
 chassis.robot_rev(PWMFREQ)
-robot_stop()
+chassis.robot_stop()
 
 chassis.robot_turn_right(PWMFREQ_LEFT, PWMFREQ_RIGHT)
-robot_stop()
+chassis.robot_stop()
 
 chassis.robot_inc_speed(MIN_DUTY_CYCLE,MAX_DUTY_CYCLE,INCREMENT_BY)
-robot_stop()
+chassis.robot_stop()
 
 chassis.robot_dec_speed(MIN_DUTY_CYCLE,MAX_DUTY_CYCLE,DECREMENT_BY)
-robot_stop()
+chassis.robot_stop()
 
 
 
